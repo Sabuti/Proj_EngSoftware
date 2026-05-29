32f1180bd582f313cf4c77ad63704e51526777c5
Sabuti <lucasa.linhares1@gmail.com>
2026-05-29 00:04:43 -0300

    melhoria do signupPage

--- proj_engSoftware/src/pages/SignupPage.jsx
+++ proj_engSoftware/src/pages/SignupPage.jsx
@@ -1,9 +1,62 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { supabase } from '../lib/supabase'
+
 function SignupPage() {
-    return (
-        <div>
-            <h1>Signup</h1>
-        </div>
-    )
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  async function handleSignup(e) {
+    e.preventDefault()
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    })
+
+    if (error) {
+      alert(error.message)
+      return
+    }
+
+    alert('Usuário criado com sucesso')
+
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <h1>Cadastro</h1>
+
+      <form onSubmit={handleSignup}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default SignupPage
