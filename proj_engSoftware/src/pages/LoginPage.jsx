d2e54cb6f0f64f92fc8379aa5735f3af291ab665
Sabuti <lucasa.linhares1@gmail.com>
2026-05-29 00:01:06 -0300

    melhoria do login

--- proj_engSoftware/src/pages/LoginPage.jsx
+++ proj_engSoftware/src/pages/LoginPage.jsx
@@ -1,9 +1,60 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { supabase } from '../lib/supabase'
+
 function LoginPage() {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  async function handleLogin(e) {
+    e.preventDefault()
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      alert(error.message)
+      return
+    }
+
+    navigate('/dashboard')
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Entrar
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default LoginPage
