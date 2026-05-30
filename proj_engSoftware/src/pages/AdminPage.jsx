ab6a399f23b8916bada680ebd66f9df611434371
Sabuti <lucasa.linhares1@gmail.com>
2026-05-30 09:42:44 -0300

    Melhoria da pagina do admin

--- proj_engSoftware/src/pages/AdminPage.jsx
+++ proj_engSoftware/src/pages/AdminPage.jsx
@@ -1,17 +1,65 @@
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabase'
+
 function AdminPage() {
-    return (
-        <div
-            style={{
-                maxWidth: '800px',
-                margin: '40px auto',
-                padding: '20px',
-                border: '1px solid #ccc',
-                borderRadius: '10px'
-            }}
-            >
-            <h1>Admin Page</h1>
-        </div>
-    )
+  const [usuarios, setUsuarios] = useState([])
+  const [disciplinas, setDisciplinas] = useState([])
+  const [turmas, setTurmas] = useState([])
+
+  useEffect(() => {
+    async function carregarDados() {
+
+      const { data: listaUsuarios } = await supabase
+        .from('usuarios')
+        .select('*')
+
+      const { data: listaDisciplinas } = await supabase
+        .from('disciplinas')
+        .select('*')
+
+      const { data: listaTurmas } = await supabase
+        .from('turmas')
+        .select('*')
+
+      setUsuarios(listaUsuarios || [])
+      setDisciplinas(listaDisciplinas || [])
+      setTurmas(listaTurmas || [])
+
+    }
+
+    carregarDados()
+  }, [])
+
+  return (
+    <div style={{
+      maxWidth: '800px',
+      margin: '40px auto',
+      padding: '20px'
+    }}>
+      <h1>Painel Administrativo</h1>
+
+      <h2>Usuários</h2>
+        {usuarios.map(usuario => (
+          <div key={usuario.id}>
+            {usuario.nome} - {usuario.email} - {usuario.perfil}
+          </div>
+        ))}
+
+      <h2>Disciplinas</h2>
+        {disciplinas.map(disciplina => (
+          <div key={disciplina.id}>
+            {disciplina.nome}
+          </div>
+        ))}
+
+      <h2>Turmas</h2>
+        {turmas.map(turma => (
+          <div key={turma.id}>
+            {turma.nome}
+          </div>
+        ))}
+    </div>
+  )
 }
 
 export default AdminPage
